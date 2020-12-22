130a44560ddd1e8ca8c17ade01952f23326ca44f
SMilton <stuart.milton@atos.net>
2020-12-22 20:17:35 +0000

    More

--- strava.py
+++ strava.py
@@ -1,27 +1,19 @@
-
-from pandas.io.json import json_normalize
 import pandas as pd
 import os
-import seaborn as sns
-
-import matplotlib.pyplot as plt
-
-import numpy as np
 
 from datetime import datetime
-
 from stravaio import strava_oauth2
 from stravaio import StravaIO
-
-import json
 
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET_ID = os.environ['CLIENT_SECRET_ID']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 
+
 def get_client(access_token=ACCESS_TOKEN):
     client = StravaIO(access_token=access_token)
     return client
+
 
 def get_athlete(ourclient):
     athlete = ourclient.get_logged_in_athlete()
@@ -31,6 +23,7 @@ def get_athlete(ourclient):
         ourclient = StravaIO(access_token=oauth2['access_token'])
         athlete = ourclient.get_logged_in_athlete()
     return athlete
+
 
 def get_all_activities(ourclient):
     activities = ourclient.get_logged_in_athlete_activities(after='2020-10-26')
@@ -45,7 +38,7 @@ def main():
 
     lejog  = pd.DataFrame(columns=['Date','Distance'])
 
-    for a in activities:
+    for a in activities :
         if a.type in ['Run', 'Walk', 'Hike'] :
             miles = a.distance / 1000 / 8 * 5
             lejog = lejog.append({'Date': a.start_date_local.date(), 'Distance': miles}, ignore_index=True)
